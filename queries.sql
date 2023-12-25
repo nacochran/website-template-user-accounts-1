69643c9b416c1bc7a36e5f35614ea76a1a29f22d
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2023-12-25 10:31:11 -0600

    Added queries.sql

---
+++ queries.sql
@@ -0,0 +1,11 @@
+-- Drop the table if it exists
+DROP TABLE IF EXISTS users;
+
+-- Create a user table
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
